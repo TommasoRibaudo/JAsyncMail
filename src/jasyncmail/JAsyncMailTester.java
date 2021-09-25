f209bf6fc9ce029c1d1b9e03943a601201314b1e
Tommaso Ribaudo <tommasoribaudo1@gmail.com>
2021-09-25 09:15:27 -0600

    Update JAsyncMailTester.java

--- src/jasyncmail/JAsyncMailTester.java
+++ src/jasyncmail/JAsyncMailTester.java
@@ -1,7 +1,3 @@
-Expression licensePrefix is undefined on line 4, column 3 in Templates/Licenses/license-default.txt.To change this license header, choose License Headers in Project Properties.
-Expression licensePrefix is undefined on line 5, column 3 in Templates/Licenses/license-default.txt.To change this template file, choose Tools | Templates
-Expression licensePrefix is undefined on line 6, column 3 in Templates/Licenses/license-default.txt.and open the template in the editor.
-
 package jasyncmail;
 
 /**
@@ -15,7 +11,7 @@ public class JAsyncMailTester
 
     public static void main(String[] args) 
     {
-
+        System.out.println("Hello World");
     }
 
 }
